46915f5cc9277e0e6ec72613db1516b85ebcbe20
Philipp Hock <philipp.hock@uni-ulm.de>
2014-02-21 14:41:44 +0100

    removed unused imports

--- src/de/uniulm/bagception/BootLoader.java
+++ src/de/uniulm/bagception/BootLoader.java
@@ -1,10 +1,9 @@
 package de.uniulm.bagception;
 
-import de.uniulm.bagception.service.CaseOpenService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import de.uniulm.bagception.service.CaseOpenService;
 
 public class BootLoader extends BroadcastReceiver{
 
